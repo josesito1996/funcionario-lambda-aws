80dd6e4efaebb1d582f39c211b408e1f6b5c914f
jose <jose@gmail.com>
2021-06-21 19:34:53 -0500

    update origins

--- src/main/java/com/javatechie/aws/lambda/config/CorsConfig.java
+++ src/main/java/com/javatechie/aws/lambda/config/CorsConfig.java
@@ -11,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+		registry.addMapping("/**")
+		.allowedOrigins("*")
+		.allowedMethods("GET", "POST", "PUT");
 	}
 
 }
