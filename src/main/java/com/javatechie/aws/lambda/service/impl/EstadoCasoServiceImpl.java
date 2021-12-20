cf8feb23cd7fc6ca9e3c0b09110c6d3d42abc44a
Jose Castillo <jcastillo@example.com>
2021-12-20 18:29:55 -0500

    ordenando estados de caso

--- src/main/java/com/javatechie/aws/lambda/service/impl/EstadoCasoServiceImpl.java
+++ src/main/java/com/javatechie/aws/lambda/service/impl/EstadoCasoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.javatechie.aws.lambda.service.impl;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,36 +14,37 @@ import com.javatechie.aws.lambda.respository.RepoEstadoCaso;
 import com.javatechie.aws.lambda.service.EstadoCasoService;
 
 @Service
-public class EstadoCasoServiceImpl extends CrudImpl<EstadoCaso, String>
-        implements EstadoCasoService {
+public class EstadoCasoServiceImpl extends CrudImpl<EstadoCaso, String> implements EstadoCasoService {
 
-    @Autowired
-    private RepoEstadoCaso repo;
+	@Autowired
+	private RepoEstadoCaso repo;
 
-    @Override
-    protected GenericRepo<EstadoCaso, String> getRepo() {
+	@Override
+	protected GenericRepo<EstadoCaso, String> getRepo() {
 
-        return repo;
-    }
+		return repo;
+	}
 
-    @Override
-    public List<EstadoCaso> listarActivos(Boolean estado) {
+	@Override
+	public List<EstadoCaso> listarActivos(Boolean estado) {
 
-        return repo.findByEstado(estado);
-    }
+		return repo.findByEstado(estado);
+	}
 
-    @Override
-    public List<ReactSelectResponse> listEstadosCasoByEstado(Boolean estado) {
+	@Override
+	public List<ReactSelectResponse> listEstadosCasoByEstado(Boolean estado) {
 
-        return listarActivos(estado).stream().map(this::transformReactSelect)
-                .collect(Collectors.toList());
-    }
+		return listarActivos(estado).stream()
+				.sorted(Comparator.comparing(EstadoCaso::getOrden))
+				.map(this::transformReactSelect)
+				.collect(Collectors.toList());
+	}
 
-    private ReactSelectResponse transformReactSelect(EstadoCaso estadoCaso) {
-        ReactSelectResponse response = new ReactSelectResponse();
-        response.setLabel(estadoCaso.getNombreEstado());
-        response.setValue(estadoCaso.getIdEstadoCaso());
-        response.setCampoAux(estadoCaso.getOrden().toString());
-        return response;
-    }
+	private ReactSelectResponse transformReactSelect(EstadoCaso estadoCaso) {
+		ReactSelectResponse response = new ReactSelectResponse();
+		response.setLabel(estadoCaso.getOrden() + " - " + estadoCaso.getNombreEstado());
+		response.setValue(estadoCaso.getIdEstadoCaso());
+		response.setCampoAux(estadoCaso.getOrden().toString());
+		return response;
+	}
 }
