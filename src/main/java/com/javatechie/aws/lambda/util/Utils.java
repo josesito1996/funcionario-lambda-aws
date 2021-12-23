34cba62cabdf33e9f22ec918b2ba321faf77abd7
Jose Castillo <jcastillo@example.com>
2021-12-22 20:02:22 -0500

    ajustes en el archivo WORD

--- src/main/java/com/javatechie/aws/lambda/util/Utils.java
+++ src/main/java/com/javatechie/aws/lambda/util/Utils.java
@@ -11,61 +11,74 @@ import java.util.Random;
 
 public class Utils {
 
-    public static int numberRandomGenerator(int min, int max) {
-        return new Random().nextInt(max - min) + min;
-    }
+	public static int numberRandomGenerator(int min, int max) {
+		return new Random().nextInt(max - min) + min;
+	}
 
-    public static String generateCorreo(String name) {
-        return name.replaceAll(" ", "").toLowerCase().concat("@gmail.com");
-    }
+	public static String generateCorreo(String name) {
+		return name.replaceAll(" ", "").toLowerCase().concat("@gmail.com");
+	}
 
-    public static LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
-        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-    }
+	public static LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+		return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
 
-    public static Date convertToDateViaInstant(LocalDate dateToConvert) {
-        return Date.from(dateToConvert.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-    }
+	public static Date convertToDateViaInstant(LocalDate dateToConvert) {
+		return Date.from(dateToConvert.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
 
-    public static Date convertToDateViaInstant(LocalDateTime dateToConvert) {
-        return Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
-    }
+	public static Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+		return Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+	}
 
-    public static String separadorDeCadenas(String cadena, String caracter, int index) {
-        if (cadena != null) {
-            if (!cadena.isEmpty()) {
-                String[] array = cadena.split(caracter);
-                if (array != null) {
-                    if (index < array.length) {
-                        return array[index];
-                    }
-                }
-            }
-        }
-        return "";
-    }
+	public static String separadorDeCadenas(String cadena, String caracter, int index) {
+		if (cadena != null) {
+			if (!cadena.isEmpty()) {
+				String[] array = cadena.split(caracter);
+				if (array != null) {
+					if (index < array.length) {
+						return array[index];
+					}
+				}
+			}
+		}
+		return "";
+	}
 
-    /**
-     * Metoddo que convierte primera letra del texto en mayuscula.
-     * 
-     * @param texto
-     * @return
-     */
-    public static String primeraLetraMayuscula(String texto) {
-        char[] arr = texto.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            if (i == 0) {
-                arr[0] = Character.toUpperCase(arr[0]);
-            } else {
-                arr[i] = Character.toLowerCase(arr[i]);
-            }
-        }
-        return new String(arr);
-    }
-    
-    public static String convertirNumeroAMes(Integer numeroMes) {
-        Locale locale = new Locale("es", "ES");
-        Month mes = Month.of(numeroMes);
-        return mes.getDisplayName(TextStyle.SHORT_STANDALONE, locale).toUpperCase();
-    }
+	/**
+	 * Metoddo que convierte primera letra del texto en mayuscula.
+	 * 
+	 * @param texto
+	 * @return
+	 */
+	public static String primeraLetraMayuscula(String texto) {
+		char[] arr = texto.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (i == 0) {
+				arr[0] = Character.toUpperCase(arr[0]);
+			} else {
+				arr[i] = Character.toLowerCase(arr[i]);
+			}
+		}
+		return new String(arr);
+	}
+
+	public static String convertirNumeroAMes(Integer numeroMes) {
+		Locale locale = new Locale("es", "ES");
+		Month mes = Month.of(numeroMes);
+		return mes.getDisplayName(TextStyle.SHORT_STANDALONE, locale).toUpperCase();
+	}
+
+	/*
+	 * Funcion que redondea numeros
+	 */
+	public static Double redondearDecimales(double valorInicial, int numeroDecimales) {
+		double parteEntera, resultado;
+		resultado = valorInicial;
+		parteEntera = Math.floor(resultado);
+		resultado = (resultado - parteEntera) * Math.pow(10, numeroDecimales);
+		resultado = Math.round(resultado);
+		resultado = (resultado / Math.pow(10, numeroDecimales)) + parteEntera;
+		return resultado;
+	}
 }
--- src/main/java/com/javatechie/aws/lambda/util/WordUtil.java
+++ src/main/java/com/javatechie/aws/lambda/util/WordUtil.java
@@ -1,8 +1,8 @@
 package com.javatechie.aws.lambda.util;
 
+import static com.javatechie.aws.lambda.util.Utils.redondearDecimales;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +32,7 @@ public class WordUtil {
 			titleRun.setFontSize(18);
 			XWPFParagraph subTitle = document.createParagraph();
 			XWPFRun subTitleRun = subTitle.createRun();
-			subTitleRun.setText("Orden de inspección : " + response.getOrdenInspeccion());
+			subTitleRun.setText("Orden de inspección: " + response.getOrdenInspeccion());
 			subTitleRun.setBold(true);
 			/*
 			 * Para la tabla
@@ -41,7 +41,10 @@ public class WordUtil {
 			XWPFParagraph subTitleTable1 = document.createParagraph();
 			XWPFRun subTitleTableRun1 = subTitleTable1.createRun();
 			subTitleTableRun1.setText("Informe Ejecutivo");
+			subTitleTableRun1.setBold(true);
+			
 			XWPFTable table1 = document.createTable();
+			table1.setWidth("100%");
 			XWPFTableRow tableRowOne1 = table1.getRow(0);
 			tableRowOne1.getCell(0).setText("Fecha Inicio");
 			tableRowOne1.addNewTableCell().setText(response.getFechaInicio());
@@ -58,8 +61,11 @@ public class WordUtil {
 			// create table 2
 			XWPFParagraph subTitleTable2 = document.createParagraph();
 			XWPFRun subTitleTableRun2 = subTitleTable2.createRun();
-			subTitleTableRun2.setText("Analisis de riesgo :");
+			subTitleTableRun2.setBold(true);
+			
+			subTitleTableRun2.setText("Análisis de riesgo:");
 			XWPFTable table2 = document.createTable();
+			table2.setWidth("100%");
 			XWPFTableRow tableRowOne2 = table2.getRow(0);
 			tableRowOne2.getCell(0).setText("Origen del Caso");
 			tableRowOne2.addNewTableCell().setText(response.getOrdenInspeccion());
@@ -68,7 +74,7 @@ public class WordUtil {
 			tableRowTwo2.getCell(1).setText(response.getTrabajadoresInvolucrados().toString());
 			XWPFTableRow tableRowThree2 = table2.createRow();
 			tableRowThree2.getCell(0).setText("Multa Potencial");
-			tableRowThree2.getCell(1).setText(response.getMultaPotencial().toString());
+			tableRowThree2.getCell(1).setText(redondearDecimales(response.getMultaPotencial(), 2).toString());
 			XWPFTableRow tableRowFour2 = table2.createRow();
 			tableRowFour2.getCell(0).setText("Provisiones");
 			tableRowFour2.getCell(1).setText(response.getProvisiones());
@@ -80,7 +86,10 @@ public class WordUtil {
 			XWPFParagraph subTitleTable3 = document.createParagraph();
 			XWPFRun subTitleTableRun3 = subTitleTable3.createRun();
 			subTitleTableRun3.setText("Actuaciones");
+			subTitleTableRun3.setBold(true);
+			
 			XWPFTable table3 = document.createTable();
+			table3.setWidth("100%");
 			XWPFTableRow tableRowOne3 = table3.getRow(0);
 			tableRowOne3.getCell(0).setText("Fecha");
 			tableRowOne3.addNewTableCell().setText("Tipo Actuación");
@@ -97,76 +106,6 @@ public class WordUtil {
 		} catch (Exception e) {
 			log.info("Exception {}", e);
 			return null;
-		}
-	}
-
-	public static void main(String... args) {
-		try {
-			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-			XWPFDocument document = new XWPFDocument();
-			XWPFParagraph title = document.createParagraph();
-			XWPFRun titleRun = title.createRun();
-			titleRun.setText("Denominacion del Caso");
-			titleRun.setFontSize(18);
-			XWPFParagraph subTitle = document.createParagraph();
-			XWPFRun subTitleRun = subTitle.createRun();
-			subTitleRun.setText("Orden de inspeccion : 2323423423423");
-			subTitleRun.setBold(true);
-			/*
-			 * Para la tabla
-			 */
-			// create table 1
-			XWPFParagraph subTitleTable1 = document.createParagraph();
-			XWPFRun subTitleTableRun1 = subTitleTable1.createRun();
-			subTitleTableRun1.setText("Informe Ejecutivo :");
-			XWPFTable table1 = document.createTable();
-			XWPFTableRow tableRowOne1 = table1.getRow(0);
-			tableRowOne1.getCell(0).setText("Fecha Inicio");
-			tableRowOne1.addNewTableCell().setText("01/02/2020");
-			XWPFTableRow tableRowTwo1 = table1.createRow();
-			tableRowTwo1.getCell(0).setText("Estado del caso");
-			tableRowTwo1.getCell(1).setText("Concluido");
-			XWPFTableRow tableRowThree1 = table1.createRow();
-			tableRowThree1.getCell(0).setText("Etapa");
-			tableRowThree1.getCell(1).setText("Sancionadora");
-			XWPFTableRow tableRowFour1 = table1.createRow();
-			tableRowFour1.getCell(0).setText("Resumen");
-			tableRowFour1.getCell(1).setText("Este es un resumen de prueba");
-
-			// create table 2
-			XWPFParagraph subTitleTable2 = document.createParagraph();
-			XWPFRun subTitleTableRun2 = subTitleTable2.createRun();
-			subTitleTableRun2.setText("Analisis de riesgo :");
-			XWPFTable table2 = document.createTable();
-			XWPFTableRow tableRowOne2 = table2.getRow(0);
-			tableRowOne2.getCell(0).setText("Origen del Caso");
-			tableRowOne2.addNewTableCell().setText("Ejemplo de origen");
-			XWPFTableRow tableRowTwo2 = table2.createRow();
-			tableRowTwo2.getCell(0).setText("Trabajadores involucrados");
-			tableRowTwo2.getCell(1).setText("15");
-			XWPFTableRow tableRowThree2 = table2.createRow();
-			tableRowThree2.getCell(0).setText("Multa Potencial");
-			tableRowThree2.getCell(1).setText("3500");
-			XWPFTableRow tableRowFour2 = table2.createRow();
-			tableRowFour2.getCell(0).setText("Provisiones");
-			tableRowFour2.getCell(1).setText("Ejemplo de provisiones");
-			XWPFTableRow tableRowFive2 = table2.createRow();
-			tableRowFive2.getCell(0).setText("Riesgo");
-			tableRowFive2.getCell(1).setText("ALTO");
-
-			// create table 3
-			XWPFParagraph subTitleTable3 = document.createParagraph();
-			XWPFRun subTitleTableRun3 = subTitleTable3.createRun();
-			subTitleTableRun3.setText("Actuaciones");
-			XWPFTable table3 = document.createTable();
-			XWPFTableRow tableRowOne3 = table3.getRow(0);
-			tableRowOne3.getCell(0).setText("Fecha");
-			tableRowOne3.addNewTableCell().setText("Tipo Actuación");
-
-			document.write(new FileOutputStream(new File("archivito.docx")));
-			document.close();
-		} catch (Exception e) {
-			log.info("Exception {}", e);
 		}
 	}
 }
