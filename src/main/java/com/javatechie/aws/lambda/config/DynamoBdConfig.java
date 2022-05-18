a608941f01a3c0d6bab59a86815fbc16bba5bbc8
Jose Castillo <jcastillo@example.com>
2022-05-18 11:44:28 -0500

    add Dynamo table name for enviromment

--- src/main/java/com/javatechie/aws/lambda/config/DynamoBdConfig.java
+++ src/main/java/com/javatechie/aws/lambda/config/DynamoBdConfig.java
@@ -4,6 +4,7 @@ import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRep
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -12,53 +13,67 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.javatechie.aws.lambda.respository")
+@Slf4j
 public class DynamoBdConfig {
 
-  @Value("${aws.config.region}")
-  private String region;
+	@Value("${aws.config.region}")
+	private String region;
 
-  @Value("${aws.config.access-key}")
-  private String accessKey;
+	@Value("${aws.config.access-key}")
+	private String accessKey;
 
-  @Value("${aws.config.secret-key}")
-  private String secretKey;
+	@Value("${aws.config.secret-key}")
+	private String secretKey;
 
-  @Value("${aws.config.service-endpoint}")
-  private String serviceEnpoint;
+	@Value("${aws.config.service-endpoint}")
+	private String serviceEnpoint;
 
-  @Bean
-  public AmazonDynamoDB amazonDynamoDB() {
+	@Value("${spring.profiles.active}")
+	private String enviroment;
 
-    return AmazonDynamoDBClientBuilder.standard()
-        .withEndpointConfiguration(endpointConfiguration())
-        .withCredentials(awsCredentialsProvider()).build();
-  }
-  
-  @Bean
-  public DynamoDB getDynamoDB() {
-      return new DynamoDB(amazonDynamoDB());
-  }
+	@Bean
+	public AmazonDynamoDB amazonDynamoDB() {
 
-  public AwsClientBuilder.EndpointConfiguration endpointConfiguration() {
-    return new AwsClientBuilder.EndpointConfiguration(serviceEnpoint, region);
-  }
+		return AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(endpointConfiguration())
+				.withCredentials(awsCredentialsProvider()).build();
+	}
 
-  public AWSCredentialsProvider awsCredentialsProvider() {
-    return new AWSStaticCredentialsProvider(
-        new BasicAWSCredentials(accessKey, secretKey));
-  }
-  
-  @Bean
-  public AWSLambda getAwsLambda() {
-      return AWSLambdaClientBuilder.standard().withCredentials(awsCredentialsProvider())
-              .withRegion(Regions.US_EAST_2)
-              .build();
-  }
-  
+	@Bean
+	public DynamoDB getDynamoDB() {
+		return new DynamoDB(amazonDynamoDB());
+	}
+
+	@Bean
+	@Primary
+	public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+		log.info("Enviromment {}", enviroment);
+		DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDB,
+				new DynamoDBMapperConfig.Builder().withTableNameResolver(new TableNameResolver(enviroment)).build());
+		return mapper;
+	}
+
+	public AwsClientBuilder.EndpointConfiguration endpointConfiguration() {
+		return new AwsClientBuilder.EndpointConfiguration(serviceEnpoint, region);
+	}
+
+	public AWSCredentialsProvider awsCredentialsProvider() {
+		return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+	}
+
+	@Bean
+	public AWSLambda getAwsLambda() {
+		return AWSLambdaClientBuilder.standard().withCredentials(awsCredentialsProvider()).withRegion(Regions.US_EAST_2)
+				.build();
+	}
+
 }
